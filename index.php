efcf7c84af83d14b041d4970a02b3bd0b6d5afbb
DavidANegrete <davidnegrete@g.harvard.edu>
2013-09-17 00:12:57 -0700

    Added about you and little bio

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<h1> Hello There</p>
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+ 	<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+ 	<link href='http://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet' type='text/css'>
+ 	<title>About David Negrete</title>
+ </head>
+
+ <body>
+ 	<div id = "page-wrapper">
+ 		<div id = "name">
+ 			<h1> David Negrete </h1>
+ 		</div>
+ 		<br>
+
+ 		<div id ="about" >
+
+ 			<h2>About Me</h2>
+ 			<p> Hello everyone! I am very happy to be taking this class because I want to learn how to make web applications. I have been creating HTML/CSS pages for about a year now. I am far from an expert developer. What I lack in experience I make up for in pure determination and curiosity. I am not afraid to try new things in my projects. Thus far, I have learned Java, C++, SQL, Python and Visual Basic. I am looking forward to a great semester of learning how to build web applications with PHP. My text editor of choice for this class is Sublime Text Version 2.0.2.  
+ 			</p>	
+ 		
+ 			<h2>Fun and Family</h2>
+ 			<p>When I am not working on projects, I either spend time with my wife and puppy or train. I love doing activities that test my endurance. I have done four marathons in the last four years and will be doing my fifth in February. I recently got into cycling and swimming. I completed my first triathlon two weeks ago and got second place in my age group and class. This coming week I will be competing in a 100K bike race.   
+			</p>
+		</div>
+	</div>
+ 
